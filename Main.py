fc01e947cb0ae5eb408259fb9e2b6e4cdebdc5bf
trexgamedev <noahtupper101@gmail.com>
2025-10-14 19:56:49 -0500

    modified:   Main.py

--- Main.py
+++ Main.py
@@ -2,3 +2,13 @@ import pygame as py
 
 py.init()
 screen = py.display.set_mode((500,500))
+py.display.set_caption("Pixel Rush")
+clock = py.time.Clock()
+running = True
+while running:
+    for event in py.event.get():
+        if event.type == py.QUIT:
+            running = False
+
+    py.display.flip()
+    clock.tick(60)
